f0ee3ef664809528dfd0ae61ca464e700e7b764e
OtoMihalik <otomihalik100@gmail.com>
2024-06-06 11:41:26 +0200

    nova databaza/connect

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "userdb";
 $password = "databaza";
-$dbname = "northwindsql";
+$dbname = "northwind";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
